b4fa9587434668928b8310c3d312a741711a629f
Claire Parker <claire.parker87@gmail.com>
2016-02-11 21:02:01 +0000

    :heavy_division_sign: Test file contents

--- test/app.js
+++ test/app.js
@@ -3,17 +3,20 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-clekyll:app', function () {
+describe('generator-clekyll:app', function() {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({postTitle: 'bananas', published: true, comments: true})
+      .withPrompts({postTitle: 'Bananas', published: true, comments: true})
       .on('end', done);
   });
 
-  it('creates a file', function () {
+  it('creates a file', function() {
     var d = (new Date()).toISOString().slice(0, 10);
     assert.file([
       d + '-bananas.markdown'
     ]);
+    assert.fileContent(d + '-bananas.markdown',
+      /layout: post\ntitle: Bananas\ndate: \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\ncomments: true\npublished: false/
+    );
   });
 });
